5ea7cdea2f36969c8bea6fe7bec8c5bc2a4dc70e
liquor <1956810010@qq.com>
2020-11-25 23:08:42 +0800

    增加单链表插入和删除函数

--- 软件工程/单链表操作.cpp
+++ 软件工程/单链表操作.cpp
@@ -27,6 +27,33 @@ linklist *creatlist()
 	r->next = NULL;
 	return head;
 }
+void delet(linklist *head,int m)
+{
+	linklist *p,*s;
+	p = head;
+	s = head->next;
+	for (int i = 0; i < m-1; i++)
+	{
+		p = p->next;
+		s = s->next;
+	}
+	p->next = s->next;
+	free(s);
+}
+void insert(linklist *head,int m,char ch)
+{
+	linklist* p, * s;
+	p = head;
+	for (int i = 0; i < m - 1; i++)
+	{
+		p = p->next;
+	}
+	s = (linklist*)malloc(sizeof(linklist));
+	s->data = ch;
+	s->next = p->next;
+	p->next = s;
+}
+
 
 char getlist(linklist *head, int i)
 {
@@ -58,7 +85,7 @@ int  main()
 		r = r->next;
 	}
 	printf("\n");
-	printf("请输入要查询的序号:");
+	printf("请输入要查询的序号(输入-1停止查找):\n");
 	scanf_s("%d", &num);
 	n = getlist(head, num);
 	while (num != -1)
@@ -71,5 +98,39 @@ int  main()
 		scanf_s("%d", &num);
 		n = getlist(head, num);
 	}
-	if (num == -1) printf("查找结束\n");
+	if (num == -1)
+		printf("查找结束\n");
+	printf("请输入要插入的序号及数据(输入0位置停止):\n");
+	scanf_s("%d %c",&num,&n);
+	while(num!=0)
+	{ 
+		insert(head, num, n);
+		printf("插入成功\n");
+		printf("请输入要插入的序号及数据:\n");
+		scanf_s("%d %c", &num,&n);
+	}
+	printf("插入结束\n");
+	r = head->next;
+	while (r)
+	{
+		printf("%c", r->data);
+		r = r->next;
+	}
+	printf("\n");
+	printf("请输入要删除的序号(输入0位置停止):\n");
+	scanf_s("%d", &num);
+	while (num!=0)
+	{
+		delet(head, num);
+		printf("删除成功\n");
+		printf("请输入要删除的序号:\n");
+		scanf_s("%d", &num);
+	}
+	printf("删除结束\n");
+	r = head->next;
+	while (r)
+	{
+		printf("%c", r->data);
+		r = r->next;
+	}
 }
